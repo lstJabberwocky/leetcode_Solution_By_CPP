8967e6be2b2f9c2955002f6f11c51144ecd8a91e
Jabberwocky <lstjabberwocky@gmail.com>
2026-03-30 09:19:42 +0800

    asd

--- 209_Minimum_Size_Subarray_Sum.cpp
+++ 209_Minimum_Size_Subarray_Sum.cpp
@@ -1,3 +1,47 @@
 //
 // Created by lenovo on 2026/3/29.
 //
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int left = 0    ,right = -1; //[left,right]
+        int sum = 0;
+        int minLength = nums.size() + 1;
+
+        while (left < nums.size())
+        {
+            if ( right +1 < nums.size() && sum < target)
+            {
+                right++;
+                sum += nums[right];
+            }
+            else
+            {
+                sum -= nums[left];
+                left++;
+            }
+            if (sum >= target)
+            {
+                minLength = min(minLength,right - left +1);
+            }
+
+        }
+        if (minLength == nums.size() + 1)
+        {
+            return  0;
+        }
+        return minLength;
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<int> v = {2,1,2,3,4,5};
+    cout << s.minSubArrayLen(12,v);
+}
